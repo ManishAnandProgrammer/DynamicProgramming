fda1a201e41f1e65f2c0808b8ae697cb9319d15d
Manish Anand <manishanandprogrammer@gmail.com>
2021-05-31 07:00:50 +0530

    Minimum Cost Path in Matrix using Tabulation

--- src/main/java/pepcoding/MinimumCostPathInMatrix.java
+++ src/main/java/pepcoding/MinimumCostPathInMatrix.java
@@ -1,13 +1,23 @@
 package pepcoding;
 
+import com.google.common.base.Stopwatch;
+
 import java.util.TreeSet;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MinimumCostPathInMatrix {
     public static void main(String[] args) {
         int[][] matrix = getMatrix();
-        int result = minimumCostWithoutDp(matrix, 0, 0, 4, 4, "");
-        System.out.println("The Result :: " + result);
+
+        Stopwatch stopwatch = Stopwatch.createUnstarted();
+        stopwatch.start();
+        System.out.println(minimumCostWithoutDp(matrix, 0, 0,
+                4, 4, ""));
+        System.out.println("Result Without DP Took:: " + stopwatch.stop());
+
+        stopwatch.reset().start();
+        System.out.println(minimumCostWithTabulation(matrix));
+        System.out.println("Result With Tabulation DP Took:: " + stopwatch.stop());
     }
 
     private static int[][] getMatrix() {
@@ -34,7 +44,7 @@ public class MinimumCostPathInMatrix {
     private static Integer minimumCostWithoutDp(int[][] matrix, int currentRow, int currentColumn,
                                             int destinationRow, int destinationColumn, String pathSoFar) {
         if(currentRow == destinationRow && currentColumn == destinationColumn) {
-            System.out.println(pathSoFar);
+            //System.out.println(pathSoFar);
             return matrix[currentRow][currentColumn];
         } else if(currentRow >= matrix.length || currentColumn >= matrix.length) {
             return null;
@@ -54,5 +64,25 @@ public class MinimumCostPathInMatrix {
             treeSet.add(verticalMoveCost + currentIndexCost);
 
         return treeSet.size() > 0 ? treeSet.first() : null;
+    }
+
+    private static Integer minimumCostWithTabulation(int[][] matrix) {
+        int[][] dpArray = new int[matrix.length][matrix.length];
+
+        for(int row = dpArray.length - 1; row >= 0; row--) {
+            for(int column = dpArray[0].length-1; column >= 0; column--) {
+                if(row == dpArray.length-1 && column == dpArray[0].length-1) {
+                    dpArray[row][column] = matrix[row][column];
+                } else if(row == dpArray.length - 1) {
+                    dpArray[row][column] = matrix[row][column] + dpArray[row][column+1];
+                } else if(column == dpArray.length - 1) {
+                    dpArray[row][column] = matrix[row][column] + dpArray[row+1][column];
+                } else {
+                    dpArray[row][column] = Math.min(dpArray[row][column+1], dpArray[row+1][column]) + matrix[row][column];
+                }
+            }
+        }
+
+        return dpArray[0][0];
     }
 }
