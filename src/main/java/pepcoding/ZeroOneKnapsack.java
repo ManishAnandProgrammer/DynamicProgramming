409d6dd3d6d6e363fd863af4d769e8d6d9e16511
Manish Anand <manishanandprogrammer@gmail.com>
2021-06-10 07:30:46 +0530

    Zero One Knapsack

--- src/main/java/pepcoding/ZeroOneKnapsack.java
+++ src/main/java/pepcoding/ZeroOneKnapsack.java
@@ -1,32 +1,42 @@
 package pepcoding;
 
+import com.google.common.base.Stopwatch;
+
+import java.util.Arrays;
+
 public class ZeroOneKnapsack {
     public static void main(String[] args) {
         int[] wightArray = {2, 5, 1, 3, 4};
         int[] priceArray = {15, 14, 10, 45, 30};
 
-        int maxWeigh = 7;
+        int maxWeigh = 11;
 
-        int result = knapsackWithoutDp(wightArray, priceArray, maxWeigh, 0, 0);
-        System.out.println(result);
+        Stopwatch stopwatch = Stopwatch.createUnstarted();
+        stopwatch.start();
+        int resultFromDp = knapsackWithDp(wightArray, priceArray, maxWeigh);
+        System.out.println(resultFromDp);
+        System.out.println("Knapsack with DP Took:: " + stopwatch.stop());
     }
 
-    public static int knapsackWithoutDp(int[] weightArray, int[] priceArray, int maxWeigh, int maxPrice, int currentItemIndex) {
-        if(maxWeigh == 0) {
-            return maxPrice;
-        }
+    public static int knapsackWithDp(int[] weightArray, int[] priceArray, int maxWeigh) {
+        int[][] dp = new int[weightArray.length + 1][maxWeigh + 1];
 
-        int finalResult = 0;
-        for(int i = currentItemIndex; i < priceArray.length; i++) {
-            if(maxWeigh >= weightArray[i]) {
-                int newWeight = maxWeigh - weightArray[i];
-                int newMaxPrice = maxPrice + priceArray[i];
-                int subPrice = knapsackWithoutDp(weightArray, priceArray, newWeight, newMaxPrice, i + 1);
-                if(subPrice > finalResult) {
-                    finalResult = subPrice;
+        for(int row = 1; row < dp.length; row++) {
+            for(int column = 1; column < dp[row].length; column++) {
+                int tryToAddWeight = weightArray[row - 1];
+                if(column >= tryToAddWeight) {
+                    int remainingWeightAfterAddingWeight = column - tryToAddWeight;
+                    int priceIfWeAddElement = priceArray[row - 1] + dp[row - 1][remainingWeightAfterAddingWeight];
+                    int priceIfWeDoNotAddElement = dp[row - 1][column];
+
+                    dp[row][column] = Math.max(priceIfWeAddElement, priceIfWeDoNotAddElement);
+                } else {
+                    dp[row][column] = dp[row - 1][column];
                 }
             }
+//            Arrays.stream(dp[row]).forEach(e -> System.out.print(e + " "));
+//            System.out.println();
         }
-        return finalResult;
+        return dp[weightArray.length][maxWeigh];
     }
 }
